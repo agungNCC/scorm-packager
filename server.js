d21c73c6a5c1ac4f2daa449f6be9f8791dec6eca
agungNCC <agung@netpolitanteam.com>
2026-03-05 15:57:58 +0700

    feat: upload limit and temp cleanup

--- server.js
+++ server.js
@@ -9,7 +9,16 @@ const app = express()
 
 app.use(express.static("public"))
 
-const upload = multer({ dest: "uploads/" })
+/*
+UPLOAD CONFIG
+limit 200MB
+*/
+const upload = multer({
+    dest: "uploads/",
+    limits: {
+        fileSize: 200 * 1024 * 1024
+    }
+})
 
 function generateId() {
     return Date.now().toString()
@@ -35,140 +44,208 @@ async function injectSCORM(htmlPath) {
     let html = await fs.readFile(htmlPath, "utf8");
 
     const injection = `
-                        <script src="SCORM_API_wrapper.min.js"></script>
-                        <script src="scorm.js"></script>
+<script src="SCORM_API_wrapper.min.js"></script>
+<script src="scorm.js"></script>
 
-                        <script>
+<script>
 
-                            const KEYWORD = "selesai";
+const KEYWORD = "selesai";
 
-                            window.addEventListener("load", function () {
+window.addEventListener("load", function () {
 
-                            document.body.addEventListener("click", function (e) {
+document.body.addEventListener("click", function (e) {
 
-                                let el = e.target;
+    let el = e.target;
 
-                                // naik ke parent max 5 level
-                                for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 5; i++) {
 
-                                if (!el) break;
+        if (!el) break;
 
-                                const text = (el.textContent || "").toLowerCase();
-                                const label = (el.getAttribute("aria-label") || "").toLowerCase();
-                                const title = (el.getAttribute("title") || "").toLowerCase();
+        const text = (el.textContent || "").toLowerCase();
+        const label = (el.getAttribute("aria-label") || "").toLowerCase();
+        const title = (el.getAttribute("title") || "").toLowerCase();
 
-                                console.log("DEBUG CLICK:", text, label, title);
+        console.log("DEBUG CLICK:", text, label, title);
 
-                                if (
-                                    text.includes(KEYWORD) ||
-                                    label.includes(KEYWORD) ||
-                                    title.includes(KEYWORD)
-                                ) {
+        if (
+            text.includes(KEYWORD) ||
+            label.includes(KEYWORD) ||
+            title.includes(KEYWORD)
+        ) {
 
-                                    console.log("SCORM COMPLETE TRIGGER");
+            console.log("SCORM COMPLETE TRIGGER");
 
-                                    if (typeof setLmsStatusCompleted === "function") {
-                                    setLmsStatusCompleted();
-                                    }
+            if (typeof setLmsStatusCompleted === "function") {
+                setLmsStatusCompleted();
+            }
 
-                                    break;
-                                }
+            break;
+        }
 
-                                el = el.parentElement;
-                                }
+        el = el.parentElement;
+    }
 
-                            }, true);
+}, true);
 
-                            });
+});
 
-                        </script>
-
-                        `
+</script>
+`
 
     html = html.replace("</body>", "\n" + injection + "\n</body>");
     await fs.writeFile(htmlPath, html);
+
     console.log("SCORM injection completed");
 }
 
 function generateManifest(entry) {
     return `
-            <?xml version="1.0" encoding="UTF-8"?>
-            <manifest identifier="BAWANA" 
-                    version="1.2"
-                    xmlns="http://www.imsproject.org/xsd/imscp_rootv1p1p2" 
-                    xmlns:adlcp="http://www.adlnet.org/xsd/adlcp_rootv1p2" 
-                    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-                    xsi:schemaLocation="http://www.imsproject.org/xsd/imscp_rootv1p1p2 imscp_rootv1p1p2.xsd  http://www.imsglobal.org/xsd/imsmd_rootv1p2p1 imsmd_rootv1p2p1.xsd  http://www.adlnet.org/xsd/adlcp_rootv1p2 adlcp_rootv1p2.xsd">
-            <metadata>
-                <schema>ADL SCORM</schema>
-                <schemaversion>1.2</schemaversion>
-            </metadata>
-            <organizations default="BAWANA">
-                <organization identifier="BAWANA">
-                    <title>⁠Scorm Player</title>
-                    <item identifier="CourseItem01" identifierref="SCO_Resource_01" isvisible="true"> 
-                        <title>⁠Scorm Player</title>
+<?xml version="1.0" encoding="UTF-8"?>
+<manifest identifier="BAWANA" 
+        version="1.2"
+        xmlns="http://www.imsproject.org/xsd/imscp_rootv1p1p2" 
+        xmlns:adlcp="http://www.adlnet.org/xsd/adlcp_rootv1p2" 
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+        xsi:schemaLocation="http://www.imsproject.org/xsd/imscp_rootv1p1p2 imscp_rootv1p1p2.xsd  http://www.imsglobal.org/xsd/imsmd_rootv1p2p1 imsmd_rootv1p2p1.xsd  http://www.adlnet.org/xsd/adlcp_rootv1p2 adlcp_rootv1p2.xsd">
 
-                    </item>
-                </organization>
-            </organizations>
-            <resources>
-                <resource identifier="SCO_Resource_01" type="webcontent" adlcp:scormtype="sco" href="genially.html"></resource>
-            </resources>
-            </manifest>
-            `
+<metadata>
+<schema>ADL SCORM</schema>
+<schemaversion>1.2</schemaversion>
+</metadata>
 
+<organizations default="BAWANA">
+<organization identifier="BAWANA">
+<title>Scorm Player</title>
+
+<item identifier="CourseItem01" identifierref="SCO_Resource_01" isvisible="true">
+<title>Scorm Player</title>
+</item>
+
+</organization>
+</organizations>
+
+<resources>
+<resource identifier="SCO_Resource_01" type="webcontent" adlcp:scormtype="sco" href="${entry}"></resource>
+</resources>
+
+</manifest>
+`
 }
 
 app.post("/convert", upload.single("file"), async (req, res) => {
 
-    const id = generateId()
+    try {
 
-    const extractPath = `extracted/${id}`
+        const id = generateId()
 
-    const outputZip = `output/${id}.zip`
+        const extractPath = `extracted/${id}`
+        const outputZip = `output/${id}.zip`
 
-    await fs.ensureDir(extractPath)
-    await fs.ensureDir("output")
+        await fs.ensureDir("uploads")
+        await fs.ensureDir("extracted")
+        await fs.ensureDir("output")
 
-    await fs.createReadStream(req.file.path)
-        .pipe(unzipper.Extract({ path: extractPath }))
-        .promise()
+        await fs.ensureDir(extractPath)
 
-    const entry = findEntryHtml(extractPath)
+        await fs.createReadStream(req.file.path)
+            .pipe(unzipper.Extract({ path: extractPath }))
+            .promise()
 
-    if (!entry) {
-        return res.status(400).send("No HTML entry found")
+        const entry = findEntryHtml(extractPath)
+
+        if (!entry) {
+            return res.status(400).send("No HTML entry found")
+        }
+
+        const htmlPath = path.join(extractPath, entry)
+
+        await injectSCORM(htmlPath)
+
+        await fs.copy("templates/imsmanifest.xml", `${extractPath}/imsmanifest.xml`)
+        await fs.copy("templates/scorm.js", `${extractPath}/scorm.js`)
+        await fs.copy("templates/SCORM_API_wrapper.min.js", `${extractPath}/SCORM_API_wrapper.min.js`)
+
+        const output = fs.createWriteStream(outputZip)
+
+        const archive = archiver("zip")
+
+        archive.pipe(output)
+
+        archive.directory(extractPath, false)
+
+        await archive.finalize()
+
+        output.on("close", () => {
+
+            res.download(outputZip)
+
+        })
+
+    } catch (err) {
+
+        console.error(err)
+        res.status(500).send("Conversion failed")
+
     }
 
-    const htmlPath = path.join(extractPath, entry)
+})
 
-    await injectSCORM(htmlPath)
+/*
+UPLOAD ERROR HANDLER
+*/
+app.use((err, req, res, next) => {
 
-    await fs.copy("templates/imsmanifest.xml", `${extractPath}/imsmanifest.xml`)
-    await fs.copy("templates/scorm.js", `${extractPath}/scorm.js`)
-    await fs.copy("templates/SCORM_API_wrapper.min.js", `${extractPath}/SCORM_API_wrapper.min.js`)
+    if (err.code === "LIMIT_FILE_SIZE") {
+        return res.status(400).send("File terlalu besar. Maksimal 200MB.")
+    }
 
-    //const manifest = generateManifest(entry)
-    //await fs.writeFile(`${extractPath}/imsmanifest.xml`, manifest)
-
-    const output = fs.createWriteStream(outputZip)
-
-    const archive = archiver("zip")
-
-    archive.pipe(output)
-
-    archive.directory(extractPath, false)
-
-    await archive.finalize()
-
-    output.on("close", () => {
-
-        res.download(outputZip)
-
-    })
+    next(err)
 
 })
+
+/*
+AUTO CLEANUP TEMP FILES
+*/
+async function cleanupTemp() {
+
+    const folders = ["uploads", "extracted", "output"]
+
+    for (const folder of folders) {
+
+        if (!fs.existsSync(folder)) continue
+
+        const files = await fs.readdir(folder)
+
+        for (const file of files) {
+
+            const filePath = path.join(folder, file)
+
+            const stat = await fs.stat(filePath)
+
+            const now = Date.now()
+
+            const age = now - stat.mtimeMs
+
+            const maxAge = 30 * 60 * 1000
+
+            if (age > maxAge) {
+
+                await fs.remove(filePath)
+
+                console.log("cleanup:", filePath)
+
+            }
+
+        }
+
+    }
+
+}
+
+/*
+RUN CLEANUP EVERY 10 MINUTES
+*/
+setInterval(cleanupTemp, 10 * 60 * 1000)
 
 app.listen(3000, () => {
 
